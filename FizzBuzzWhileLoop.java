de9e927c9583ecea32fc7328a6fc617057128d14
Chirag <chiraggarg1708@gmail.com>
2025-03-01 11:42:44 +0530

    [Chirag] ADD: Find the Fizz, Buzz and FizzBuzz numbers using while loop

---
+++ FizzBuzzWhileLoop.java
@@ -0,0 +1,38 @@
+// Creating Class with name FizzBuzzWhileLoop to print numbers with FizzBuzz logic using while loop
+import java.util.Scanner;
+
+class FizzBuzzWhileLoop {
+        public static void main(String[] args){
+                // Create a Scanner Object to take user input
+                Scanner input = new Scanner(System.in);
+
+                // Get input value for number
+                System.out.print("Enter a positive integer: ");
+                int number = input.nextInt();
+
+                // Check if the number is a positive integer
+                if (number < 1) {
+                        System.out.printf("The number %d is not a positive integer.\n", number);
+                } else {
+                        // Initialize counter
+                        int i = 1;
+
+                        // Loop from 1 to the entered number using while loop
+                        while (i <= number) {
+                                if (i % 3 == 0 && i % 5 == 0) {
+                                        System.out.println("FizzBuzz"); // Multiple of both 3 and 5
+                                } else if (i % 3 == 0) {
+                                        System.out.println("Fizz"); // Multiple of 3
+                                } else if (i % 5 == 0) {
+                                        System.out.println("Buzz"); // Multiple of 5
+                                } else {
+                                        System.out.println(i); // Print the number
+                                }
+                                i++; // Increment counter
+                        }
+                }
+
+                // Close the Scanner object to avoid resource leakage
+                input.close();
+        }
+}
